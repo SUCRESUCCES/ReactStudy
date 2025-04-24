a678cfc47c5758f8655c3b6977a286902024a4ac
밍 <sucresucces@icloud.com>
2025-04-24 11:58:56 +0900

    250424 props는 입력값이다. props에 중괄호{}를 사용하면 js의 데이터 타입으로 주입되고, ""를 사용하면 문자열로 평가된다.
    함수(컴포넌트)의 리턴값에서 중괄호를 쓰면 변수로 간주된다.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
-function Counter(){
+function Counter(props){
+  // props는 입력값이다
+  console.log(props);
   return<div>
-    <h1>카운터</h1>
-    <button>+</button> 0 
+    <h1>{props.title}</h1>  
+    {/* 중괄호 넣어서 가져옴 */}
+    <button>+</button> {props.initValue} 
   </div>
 }
 function App() {
   return (
     <div>
-      <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter>
+      <Counter title="불면증 카운터" initValue={10}></Counter>
+      <Counter title="손님 카운터" initValue={10}></Counter>
+      {/* " " 안에 데이터를 넣으면 문자열! */}
+      {/* { } 안에 넣으면 자바스크립트의 데이터 타입! */}
+      
     </div>
     );
 }
