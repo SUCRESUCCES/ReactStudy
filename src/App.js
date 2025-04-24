21e0711134c54b3516c20b77485fd6556c27178e
밍 <sucresucces@icloud.com>
2025-04-24 20:46:53 +0900

    구조 분해 할당을 이용해서 코드를 좀 간결하고, 분명하게 개선

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import "./App.css";
 import { useState } from "react";
-function Counter(props){
-  // props는 입력값이다
-  let countState = useState(props.initValue);
-  let count = countState[0];
-  let setCount = countState[1];
-  console.log(countState);
-  // 첫 번째 원소 : 상태의 값
-  // 두 번째 원소 : 상태의 값을 바꿀 때 호출하는 함수
+// function Counter(props){
+//   // props는 입력값이다
+//   let countState = useState(props.initValue);
+//   let count = countState[0];
+//   let setCount = countState[1];
+//   console.log(countState);
+//   // 첫 번째 원소 : 상태의 값
+//   // 두 번째 원소 : 상태의 값을 바꿀 때 호출하는 함수
+//   function up(){
+//     setCount(count+1);
+//     // props는 외부에서 내부로 주입되는 상태
+//     // state는 내부적으로 사용하는 상태
+//   }
+//   function down(){
+//     setCount(count-1);
+//   }
+  
+//   return<div>
+//     <h1>{props.title}</h1>  
+//     {/* 중괄호 넣어서 가져옴 */}
+//     <button onClick={up}>+</button>{count} <button onClick={down}>-</button>
+//   </div>
+// }
+
+function Counter({title,initValue}){
+// function Counter(props){
+  const [count, setCount] = useState(initValue);
+  // const countState = useState(props.initValue);
+  // const count = countState[0];
+  // const setCount = countState[1];
+
   function up(){
     setCount(count+1);
-    // props는 외부에서 내부로 주입되는 상태
-    // state는 내부적으로 사용하는 상태
   }
   function down(){
     setCount(count-1);
   }
   
   return<div>
-    <h1>{props.title}</h1>  
-    {/* 중괄호 넣어서 가져옴 */}
+    <h1>{title}</h1>  
     <button onClick={up}>+</button>{count} <button onClick={down}>-</button>
   </div>
 }
