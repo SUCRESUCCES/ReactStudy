4a95c85c416a905bf190d00485f2b90e5564016f
밍 <sucresucces@icloud.com>
2025-04-24 14:25:05 +0900

    250424 리액트의 코드는 js가 아니라 jsx이다. jsx는 Js+태그를 쉽게 코드에 포함시킬 수 있도록 고안된 jsㅎ 확장언어다.
    onclick -> onClick
    onchange -> onChange
    
    이벤트의 값은 함수다.
    
    props의 값은 read only
    State의 값은 쓰기 가능
    
    state는 배열이다.
    첫번째 원소는 값이다. 수정하면 안된다.
    두번째 원소는 함수다.
    수정할 때 호출한다. 파라미터가 변경되었을 때만 컴포넌트가 다시 실행된다.
    props, state가 바뀌면 컴포넌트가 다시 렌더링 된다.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 function Counter(props){
   // props는 입력값이다
-  console.log(props);
+  let countState = useState(props.initValue);
+  let count = countState[0];
+  let setCount = countState[1];
+  console.log(countState);
+  // 첫 번째 원소 : 상태의 값
+  // 두 번째 원소 : 상태의 값을 바꿀 때 호출하는 함수
+  function up(){
+    setCount(count+1);
+    // props는 외부에서 내부로 주입되는 상태
+    // state는 내부적으로 사용하는 상태
+  }
+  function down(){
+    setCount(count-1);
+  }
+  
   return<div>
     <h1>{props.title}</h1>  
     {/* 중괄호 넣어서 가져옴 */}
-    <button>+</button> {props.initValue} 
+    <button onClick={up}>+</button>{count} <button onClick={down}>-</button>
   </div>
 }
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={10}></Counter>
-      <Counter title="손님 카운터" initValue={10}></Counter>
       {/* " " 안에 데이터를 넣으면 문자열! */}
       {/* { } 안에 넣으면 자바스크립트의 데이터 타입! */}
       
