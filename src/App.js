3d9b379852426c333060e0337a6c65470320ad52
밍 <sucresucces@icloud.com>
2025-04-28 15:13:56 +0900

    부작용은 useEffect에 격리한다. - 부작용을 쉽게 파악할 수 있다. - 테스팅 할 때 유리하다. - 부작용의 실행 타이밍을 제어할 수 있다. 두번째 파라미터가 없으면 컴포넌트와 함께 실행된다. 빈 배열이면 딱 한번 실행된다. 값이 있으면 그 값이 변경되었을 때 실행된다.
    함수가 정의될 때 함수 내에서 사용되는 변수는 함수 안에 봉인된다. 클로저라고 한다.
    
    set 함수의 입력값은 값이거나, 함수이다. 함수의 파라미터는 신선한 상태의 값이다. 리턴값이 새로운 상태가 된다.
    
    useEffect의 리턴값은 정리할 때 사용된다. unmount, 재실행될 때 자동으로 호출된다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // function Counter(props){
 //   // props는 입력값이다
 //   const countState = useState(props.initValue);
@@ -62,12 +62,46 @@ function Counter({title,initValue}){
     </ol>
   </div>
 }
+
+function CounterUseEffect(){
+  const [count, setCount] = useState(0);
+  console.log('CounterUserEffect', count);
+  useEffect(()=>{
+    const id = setInterval(()=>{
+      setCount(oldCount => oldCount + 1); //콜백함수
+    }, 1000)
+    return () =>{   // 리턴 함수는 위의 코드를 정리할 때 쓰는 함수
+      console.log('clean');
+      clearInterval(id);
+      // useEffect의 리턴값은 정리할 때 사용!
+      // 1. 컴포넌트가 지워졌을 때
+      // 2. useEffect가 재실행될 때
+    }
+  },[]);
+  
+  
+  // 사이드 이펙트 : 부작용
+  // 사이드 이펙트는 별도로 격리한다.(예측 가능성이 떨어지기 때문에)
+  // 사이드 이펙트는 useEffect로 격리한다.
+
+  // []빈 배열을 넣으면 처음 컴포넌트가 렌더링될 때 딱 한번만 useEffect 안에 있는 코드가 실행됨
+  // []없이 작성하면 컴포넌트가 렌더링 될 때마다 계속 useEffect가 실행됨
+  // [count]로 작성하면 count가 실행될때만 실행하게 함
+
+  //set함수의 값은 값일 수도 있고 함수일 수도 있다.
+  // 함수일때는 첫번째 파라미터는 현재의 state값이다.(최신값)
+
+  return <div>
+    <h1>useEffect Counter</h1> {count}
+  </div>
+}
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={10}></Counter>
       {/* " " 안에 데이터를 넣으면 문자열! */}
       {/* { } 안에 넣으면 자바스크립트의 데이터 타입! */}
+      <CounterUseEffect></CounterUseEffect>
       
     </div>
     );
